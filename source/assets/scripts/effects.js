f73743c05beaa10d377e7217689a838f350420e1
patrick <patrickzocli2021@gmail.com>
2023-02-16 21:04:59 +0100

    Fix header bug; Add anime.js

--- source/assets/scripts/effects.js
+++ source/assets/scripts/effects.js
@@ -1,7 +1,24 @@
 $(function() {
+    nav_toggle();
+    
+});
+
+function nav_toggle() {
     let nav_toggler = $('#checkbox_toggle');
+    let nav_icon_close = $('.nav_close');
+    let nav_icon_open = $('.nav_open');
 
     nav_toggler.on('click', function() {
-        $('header nav').animate({width: 'toggle'}, 500);
-    });  
-});
+        console.log($(this).prop('checked'));
+        if ($(this).prop('checked')) {
+            $('header nav').css('width', '70vw');
+            nav_icon_close.toggle();
+            nav_icon_open.toggle();
+        } else {
+            $('header nav').css('width', 0);
+            nav_icon_close.toggle();
+            nav_icon_open.toggle();
+        }
+    });
+}
+   
